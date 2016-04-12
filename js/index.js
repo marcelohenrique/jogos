b56613556ac1145f6aa5bd8b591796c77ca1847f
marcelo.lima <marcelo.lima@DINFO-34934.trt16.gov.br>
2016-04-12 14:34:33 -0300

    Ajustes na informação de jogos anteriores.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
-$(document).ready(function() {
-	$.getJSON('json/jogos.json', function(context) {
-		$.get('pages/template_index.html', function(templateScript) {
-			var template = Handlebars.compile(templateScript);
-			var html = template(context);
-			$('#content-placeholder').html(html);
+$( document ).ready( function() {
+   $.getJSON( 'json/jogos.json', function( context ) {
+      $.get( 'pages/template_index.html', function( templateScript ) {
+         var template = Handlebars.compile( templateScript );
+         var html = template( context );
+         $( '#content-placeholder' ).html( html );
 
-			for ( var i in context.jogos) {
-				var jogo = context.jogos[i];
-				informacoes(jogo.id, jogo.id);
-			}
-		}, 'html');
-	});
-});
+         for ( var i in context.jogos ) {
+            var jogo = context.jogos[ i ];
+            informacoes( jogo.id, jogo.id );
+         }
+      }, 'html' );
+   } );
+} );
 
-function informacoes(id, nome) {
-	$('#' + id + '_data').html(
-			'<small><strong>Último:</strong> '
-					+ (localStorage.getItem(nome + '_data') == null ? 'n/a'
-							: localStorage.getItem(nome + '_data'))
-					+ '</small>');
-	$('#' + id + '_acertos').html(
-			'<small><strong>Acertos:</strong> '
-					+ (localStorage.getItem(nome + '_acertos') == null ? 'n/a'
-							: localStorage.getItem(nome + '_acertos'))
-					+ '</small>');
-	$('#' + id + '_erros').html(
-			'<small><strong>Erros:</strong> '
-					+ (localStorage.getItem(nome + '_erros') == null ? 'n/a'
-							: localStorage.getItem(nome + '_erros'))
-					+ '</small>');
+function informacoes( id, nome ) {
+   $( '#' + id + '_data' ).html(
+         '<small>'
+               + ( localStorage.getItem( nome + '_data' ) == null ? 'n/a'
+                     : localStorage.getItem( nome + '_data' ) ) + '</small>' );
+   $( '#' + id + '_acertos' )
+         .html(
+               '<small>'
+                     + ( localStorage.getItem( nome + '_acertos' ) == null ? 'n/a'
+                           : localStorage.getItem( nome + '_acertos' ) )
+                     + '</small>' );
+   $( '#' + id + '_erros' ).html(
+         '<small>'
+               + ( localStorage.getItem( nome + '_erros' ) == null ? 'n/a'
+                     : localStorage.getItem( nome + '_erros' ) ) + '</small>' );
 }
