982af94d3fe1b802561b14ae0d2534f7db398c56
Marcelo Henrique de Oliveira Lima <mhguara@gmail.com>
2016-04-14 00:05:27 -0300

    Formatação da data. Ordenação dos jogos pelos últimos jogados.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 $( document ).ready( function() {
    $.getJSON( 'json/jogos.json', function( context ) {
       $.get( 'pages/template_index.html', function( templateScript ) {
+         for ( var i in context.jogos ) {
+            var jogo = context.jogos[ i ];
+            jogo.data = new Date( localStorage.getItem( jogo.id + '_data' ) );
+         }
+
+         context.jogos.sort( function( a, b ) {
+            return a.data.getTime() - b.data.getTime();
+         } );
+
          var template = Handlebars.compile( templateScript );
          var html = template( context );
          $( '#content-placeholder' ).html( html );
@@ -13,11 +22,22 @@ $( document ).ready( function() {
    } );
 } );
 
+var dias = [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ];
+
+function formata( dataStr ) {
+   var data = new Date( dataStr );
+   return dias[ data.getDay() ] + ', ' + data.getDate() + '/'
+         + ( data.getMonth() < 9 ? '0' : '' ) + ( data.getMonth() + 1 ) + '/'
+         + data.getFullYear() + ' ' + data.getHours() + ':' + data.getMinutes()
+         + ':' + data.getSeconds() + '.';
+}
+
 function informacoes( id, nome ) {
    $( '#' + id + '_data' ).html(
          '<small>'
                + ( localStorage.getItem( nome + '_data' ) == null ? 'n/a'
-                     : localStorage.getItem( nome + '_data' ) ) + '</small>' );
+                     : formata( localStorage.getItem( nome + '_data' ) ) )
+               + '</small>' );
    $( '#' + id + '_acertos' )
          .html(
                '<small>'
